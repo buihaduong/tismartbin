6727551691d7f2f969898b9f7dd423bf5ca96a74
Bui Ha Duong <buihaduong@yahoo.com.vn>
2014-09-27 19:49:40 +0700

    update code

--- firebase_test.py
+++ firebase_test.py
@@ -1,7 +1,11 @@
 import requests
 import json
 
-r = requests.get('https://incandescent-fire-7887.firebaseio.com/location1.json')
+LOCATION = 'location1'
+BASE_URL = 'https://incandescent-fire-7887.firebaseio.com/'
+DATA_URL = BASE_URL + LOCATION + '.json'
+
+r = requests.get(DATA_URL)
 
 print r.json()
 
@@ -11,6 +15,6 @@ data['trash'] = 20
 
 print type(data)
 
-r = requests.put('https://incandescent-fire-7887.firebaseio.com/location1.json', data=json.dumps(data))
+r = requests.put(DATA_URL, data=json.dumps(data))
 
 print r.text
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-'''
-    Simple socket server using threads
-'''
- 
+import requests
+import json
 import socket
 import sys
 from thread import *
  
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
- 
+
+LOCATION = 'location1'
+BASE_URL = 'https://incandescent-fire-7887.firebaseio.com/'
+DATA_URL = BASE_URL + LOCATION + '.json'
+
+r = requests.get(DATA_URL) 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 print 'Socket created'
@@ -25,7 +29,16 @@ print 'Socket bind complete'
 #Start listening on socket
 s.listen(10)
 print 'Socket now listening'
- 
+
+def updateData(data):
+    global r
+    new_data = r.json()
+    if data[0] == 'n':
+        new_data['temp'] = float(data[1:])
+    if data[0] == 'r':
+        new_data['trash'] = float(data[1:])
+    r = requests.put(DATA_URL, data=json.dumps(new_data))
+
 #Function for handling connections. This will be used to create threads
 def clientthread(conn, addr):
     #Sending message to connected client
@@ -37,10 +50,12 @@ def clientthread(conn, addr):
         #Receiving from client
         data = conn.recv(1024)
         reply = 'OK...' + data
+        
         if not data: 
             break
         if data == 'exit\r\n':
         	break
+        updateData(data)
      
         conn.sendall(reply)
    
